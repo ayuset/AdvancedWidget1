d4f99ab212e8fd439c13ac11df9990e1e6900c2e
ayu <ayusetyaningtyas16@gmail.com>
2016-10-12 18:24:08 +0700

    Initial commit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -2,12 +2,19 @@ package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LinearLayout llMain = (LinearLayout) findViewById(R.id.linearLayoutMain);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText etNama = new EditText(this);
+        llMain.addView(etNama);
     }
 }
